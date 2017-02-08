10a409058891dd27a64228fd777a57b3f3773f1e
himanshu-tripathi <himanshusgsits.tc@gmail.com>
2017-02-08 10:31:53 -0600

    2nd commit

--- src/main/java/com/Himanshu/eaiRequestController.java
+++ src/main/java/com/Himanshu/eaiRequestController.java
@@ -1,5 +1,8 @@
 package com.Himanshu;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
@@ -12,6 +15,17 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class eaiRequestController {
 
+	public String getEAI() {
+		return EAI;
+	}
+
+	public void setEAI(String eAI) {
+		EAI = eAI;
+	}
+
+
+	private String EAI;
+	
 	@GetMapping("/eaiRequest")
 	public String eaiReq(Model model) {
 		model.addAttribute("eaiRequest", new eaiRequest());
@@ -20,9 +34,17 @@ public class eaiRequestController {
 
 	@PostMapping("/eaiRequest")
 	public String eaiSubmit(@ModelAttribute eaiRequest eaiRequest) {
+		setEAI(eaiRequest.getEAI());
 		return "result";
 	}
-
+	
+	public String callRestAPI(String eai){
+		Map<String, Long> vars = new HashMap<String, Long>();
+		vars.put("eai", Long.parseLong(eai));
+		/*ReturnClass obj = restTemplate.getForObject(
+		  "REST URL", ReturnClass.class, vars);*/
+		return null;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(eaiRequestController.class, args);
